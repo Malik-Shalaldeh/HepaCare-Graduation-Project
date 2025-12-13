1af7de5ce588a435d7ee41e765970cb159b927b9
Malikshalaldeh <22210495@students.hebron.edu>
2025-12-13 15:26:53 +0200

    commit

--- AbedEndPoint.js
+++ AbedEndPoint.js
@@ -2,7 +2,7 @@
 import { Platform } from "react-native";
 
 //const BASE_URL = "https://luminous-possibility-production.up.railway.app";
-const BASE_URL = "https://a860581e-ff37-4cca-a5df-9899b5f5190d-00-21pazcdxx3mej.pike.replit.dev";
+const BASE_URL = "http://192.168.1.3:8000";
 const AbedEndPoint = {
   BASE_URL,
 
--- samiendpoint.js
+++ samiendpoint.js
@@ -3,7 +3,7 @@
 // This makes maintaining URL changes easy and avoids hard-coding strings.
 
 //const BASE_URL = "https://luminous-possibility-production.up.railway.app";
-const BASE_URL = "https://a860581e-ff37-4cca-a5df-9899b5f5190d-00-21pazcdxx3mej.pike.replit.dev";
+const BASE_URL = "http://192.168.1.3:8000";  // افتراض أن FastAPI يعمل على هذا البورت
 const ENDPOINTS = {
   // الاتصال الأساسي
   BASE_URL,
