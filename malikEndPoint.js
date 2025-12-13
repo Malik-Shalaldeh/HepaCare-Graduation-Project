c0790085b7933e0560d97380a58d4b077db45f6e
Malikshalaldeh <22210495@students.hebron.edu>
2025-12-13 13:32:11 +0200

    maintain

--- malikEndPoint.js
+++ malikEndPoint.js
@@ -1,6 +1,6 @@
 // malikEndPoint.js
 //const BASE_URL = "https://luminous-possibility-production.up.railway.app";
-const BASE_URL = "http://192.168.1.10:8000";
+const BASE_URL = "https://a860581e-ff37-4cca-a5df-9899b5f5190d-00-21pazcdxx3mej.pike.replit.dev";
 
 const ENDPOINTS = {
   BASE_URL,
