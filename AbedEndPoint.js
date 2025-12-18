5b39f9a33d2479eeb664ededf81ff87ec3d37ae7
Sami Ghaith <samighaith37@gmail.com>
2025-12-18 11:25:27 +0200

    take github version for endpoint files

--- AbedEndPoint.js
+++ AbedEndPoint.js
@@ -1,21 +1,6 @@
 // AbedEndPoint.js
-import { Platform } from "react-native";
-
-// ✅ غيّر هذا الـ IP ليكون IP جهازك اللي شغال عليه السيرفر (Django/FastAPI/...)
-// مثال: ipconfig -> IPv4 Address
-const DEV_MACHINE_IP = "192.168.1.122";
-
-// ✅ Android Emulator لازم 10.0.2.2 بدل localhost
-const ANDROID_EMULATOR_BASE = "http://10.0.2.2:8000";
-
-// ✅ للأجهزة الفعلية (موبايل على نفس الشبكة)
-const LAN_BASE = `http://${DEV_MACHINE_IP}:8000`;
-
-// ✅ اختر تلقائياً
-// - Android Emulator => 10.0.2.2
-// - غير هيك => IP الشبكة
-const BASE_URL =
-  Platform.OS === "android" ? ANDROID_EMULATOR_BASE : LAN_BASE;
+//const BASE_URL = "http://10.13.80.148:8000";
+const BASE_URL = "https://dyan-cacciatore-softer.ngrok-free.dev";
 
 const AbedEndPoint = {
   BASE_URL,
@@ -31,7 +16,7 @@ const AbedEndPoint = {
   labDashboardById: (id) => `${BASE_URL}/lab/dashboard/${id}`,
   labResultsByPatient: (id) => `${BASE_URL}/lab/results/${id}`,
 
-  // Medications (catalog)
+  // Medications
   medicationsList: `${BASE_URL}/medications`,
 
   // Patient medications
@@ -45,12 +30,9 @@ const AbedEndPoint = {
   // Patients
   patientCities: `${BASE_URL}/doctor/cities`,
   patientById: (id) => `${BASE_URL}/patient/patients/${id}`,
-  patientCreate: `${BASE_URL}/doctor/patients`, // ✅ مستخدم في Step1
+  patientCreate: `${BASE_URL}/doctor/patients`,
 
-  // ❌ محذوف لأنه ما عاد في endpoint /patient/search
-  // patientSearch: `${BASE_URL}/patient/search`,
-
-  // InputTestResultScreen
+  // Tests
   testsList: `${BASE_URL}/tests/list`,
   patientsSearch: `${BASE_URL}/patients/search`,
   inputResultSave: `${BASE_URL}/input-result/save`,
